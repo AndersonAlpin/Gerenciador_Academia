a21e240dce01e6dc716ea930b8820cfc98e182e8
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-03 13:38:43 -0300

    Novo estilo na home

--- index.js
+++ index.js
@@ -18,8 +18,13 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/home", (req, res) => {
-    res.render("home");
+app.post("/home", (req, res) => {
+    var email = req.body.email;
+    var senha = req.body.senha;
+    res.render("home", {
+        email: email,
+        senha: senha
+    });
 });
 
 app.get("/clientes", (req, res) => {
