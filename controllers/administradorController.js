ff32f2411040f2e0f35c34646d6b6e4278670782
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-15 14:56:10 -0300

    Pagamentos atrasados(tela administrador) funcionando

--- controllers/administradorController.js
+++ controllers/administradorController.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Cliente = require("../models/Cliente");
+const Mensalidade = require("../models/Mensalidade");
 
 router.get("/administrador/", (req, res) => {
     var numClientes = 0;
+    var numAtraso = 0;
+
+    Mensalidade.findAll({
+        where: {status: 'Em atraso'}
+    }).then(mensalidades => {
+        mensalidades.forEach(mensalidade => {
+            numAtraso++;
+        });
+    });
+
     Cliente.findAll().then(clientes => {
         clientes.forEach(cliente => {
             numClientes++;
         });
-        res.render("administrador/", {numClientes: numClientes});
+        res.render("administrador/", {numClientes: numClientes, numAtraso: numAtraso});
     });
 });
 
