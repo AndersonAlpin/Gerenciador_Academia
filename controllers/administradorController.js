0a60a4d6cb35117384a453cdd6b867be609cee3d
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-15 12:07:38 -0300

    Clientes ativos tela adm funcionando

--- controllers/administradorController.js
+++ controllers/administradorController.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Cliente = require("../models/Cliente");
 
 router.get("/administrador/", (req, res) => {
-    res.render("administrador/");
+    var numClientes = 0;
+    Cliente.findAll().then(clientes => {
+        clientes.forEach(cliente => {
+            numClientes++;
+        });
+        res.render("administrador/", {numClientes: numClientes});
+    });
 });
 
 module.exports = router;
