1dcc5d5aded449ba1f0cdca6bf7256186167dc3f
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-01 10:20:51 -0300

    Testando novo design

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,36 @@
 // =============================
 // ===========CLIENTE===========
-//CADASTRO CLIENTE 
-document.getElementById("selectPacote").addEventListener("change", selecionarPacote);
+//CADASTRO CLIENTE
+var pacote = document.getElementById("selectPacote");
+
+if(pacote){
+    document.getElementById("selectPacote").addEventListener("change", selecionarPacote);
+}
+
 function selecionarPacote() {
     var select = document.getElementById("selectPacote");
     var selecionado = select.options[select.selectedIndex].value;
     document.getElementById("inputPacote").value = selecionado;
 }
 
-document.getElementById("selectUF").addEventListener("change", selecionarUF);
+var UF = document.getElementById("selectUF");
+
+if(UF){
+    document.getElementById("selectUF").addEventListener("change", selecionarUF);
+}
+
 function selecionarUF() {
     var selectUF = document.getElementById("selectUF");
     var selecionadoUF = selectUF.options[selectUF.selectedIndex].text;
     document.getElementById("inputUF").value = selecionadoUF;
 }
 
-document.getElementById("selectPagamento").addEventListener("change", selectPagamento);
+var pagamento = document.getElementById("selectPagamento");
+
+if(pagamento){
+    document.getElementById("selectPagamento").addEventListener("change", selectPagamento);
+}
+
 function selectPagamento() {
     var selectPagamento = document.getElementById("selectPagamento");
     var selecionandoPagamento = selectPagamento.options[selectPagamento.selectedIndex].text;
