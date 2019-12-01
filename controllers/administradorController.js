265f6159d6bf9b36f2f833fb8afc4ca49a73054a
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-01 12:08:31 -0300

    Adicionado ''Entradas do mês''

--- controllers/administradorController.js
+++ controllers/administradorController.js
@@ -6,6 +6,7 @@ const Mensalidade = require("../models/Mensalidade");
 router.get("/administrador/", (req, res) => {
     var numClientes = 0;
     var numAtraso = 0;
+    var totalEntradas = 0;
 
     // MENSALIDADES ATRASADAS
     Mensalidade.findAll({
@@ -16,13 +17,24 @@ router.get("/administrador/", (req, res) => {
         });
     });
 
+    // TOTAL DE ENTRADAS
+    Mensalidade.findAll({
+        where: {status: 'Pago'}
+    }).then(valores => {
+        valores.forEach(valor => {
+            totalEntradas += valor.valor
+        });
+    });
+
+
     // CLIENTES ATIVOS
     Cliente.findAll().then(clientes => {
         clientes.forEach(cliente => {
             numClientes++;
         });
-        res.render("administrador/", {numClientes: numClientes, numAtraso: numAtraso});
+        res.render("administrador/", {numClientes: numClientes, numAtraso: numAtraso, totalEntradas: totalEntradas});
     });
 });
+
 
 module.exports = router;
