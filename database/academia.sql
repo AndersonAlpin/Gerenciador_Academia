4559e9c02ed90e1ff3c59c4cc26d847f59fb6a8c
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-02 02:47:51 -0300

    Base de dados atualizada

--- database/academia.sql
+++ database/academia.sql
@@ -62,7 +62,7 @@ CREATE TABLE `administradors` (
   PRIMARY KEY (`id`),
   KEY `AcademiumId` (`AcademiumId`),
   CONSTRAINT `administradors_ibfk_1` FOREIGN KEY (`AcademiumId`) REFERENCES `academia` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +71,7 @@ CREATE TABLE `administradors` (
 
 LOCK TABLES `administradors` WRITE;
 /*!40000 ALTER TABLE `administradors` DISABLE KEYS */;
+INSERT INTO `administradors` VALUES (1,'Admin','IronFit','045.123.451-15','(73)95543-1256','2019-12-01 19:19:53','2019-12-01 19:19:53',1);
 /*!40000 ALTER TABLE `administradors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,7 +100,7 @@ CREATE TABLE `clientes` (
   KEY `pacoteId` (`pacoteId`),
   CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`AcademiumId`) REFERENCES `academia` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `clientes_ibfk_2` FOREIGN KEY (`pacoteId`) REFERENCES `pacotes` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,6 +109,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Raul','Seixas','1333-02-12','352.131.214-45','(87)44444-5555','df@gmail.com',1,'2019-11-18 16:10:40','2019-11-29 16:33:09',1,1),(2,'Zé','Ramalho','1994-07-11','124.144.124-12','(44)44444-4444','email@hotmail.com',1,'2019-11-24 14:02:49','2019-11-29 16:33:09',1,1),(3,'Richie','Sambora','2005-12-12','123.123.123-12','(54)23244-4444','email@hotmail.com',1,'2019-12-01 13:14:20','2019-12-01 13:14:20',1,2);
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,7 +134,7 @@ CREATE TABLE `enderecoclientes` (
   PRIMARY KEY (`id`),
   KEY `clienteId` (`clienteId`),
   CONSTRAINT `enderecoclientes_ibfk_1` FOREIGN KEY (`clienteId`) REFERENCES `clientes` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,6 +143,7 @@ CREATE TABLE `enderecoclientes` (
 
 LOCK TABLES `enderecoclientes` WRITE;
 /*!40000 ALTER TABLE `enderecoclientes` DISABLE KEYS */;
+INSERT INTO `enderecoclientes` VALUES (1,'Rua Qualquer','124','Qualquer','Qualquer','12421414','SC','2019-11-18 16:10:40','2019-11-18 16:10:40',1),(2,'Rua Seila','12','Ubatã','Júlio Aderne','45550000','BA','2019-11-24 14:02:49','2019-11-24 14:02:49',2),(3,'Rua Qualquer','12','Ubatã','Dois de Julho','45550000','BA','2019-12-01 13:14:21','2019-12-01 13:14:21',3);
 /*!40000 ALTER TABLE `enderecoclientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +164,7 @@ CREATE TABLE `logins` (
   PRIMARY KEY (`id`),
   KEY `administradorId` (`administradorId`),
   CONSTRAINT `logins_ibfk_1` FOREIGN KEY (`administradorId`) REFERENCES `administradors` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -170,6 +173,7 @@ CREATE TABLE `logins` (
 
 LOCK TABLES `logins` WRITE;
 /*!40000 ALTER TABLE `logins` DISABLE KEYS */;
+INSERT INTO `logins` VALUES (1,'adminironfit@gmail.com','$2a$10$TSwvuD6mOjIq2f7aEnhhbOdYFr3W815KzpCWCLjRVf7UGMo4LaT7a','2019-12-01 19:19:53','2019-12-01 19:19:53',1);
 /*!40000 ALTER TABLE `logins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -184,7 +188,7 @@ CREATE TABLE `mensalidades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `valor` double NOT NULL DEFAULT '50',
   `dataEmissao` datetime NOT NULL,
-  `dataVencimento` datetime NOT NULL,
+  `dataVencimento` date DEFAULT NULL,
   `dataPagamento` datetime DEFAULT NULL,
   `formaPagamento` varchar(255) DEFAULT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Em aberto',
@@ -194,7 +198,7 @@ CREATE TABLE `mensalidades` (
   PRIMARY KEY (`id`),
   KEY `clienteId` (`clienteId`),
   CONSTRAINT `mensalidades_ibfk_1` FOREIGN KEY (`clienteId`) REFERENCES `clientes` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,6 +207,7 @@ CREATE TABLE `mensalidades` (
 
 LOCK TABLES `mensalidades` WRITE;
 /*!40000 ALTER TABLE `mensalidades` DISABLE KEYS */;
+INSERT INTO `mensalidades` VALUES (13,50,'2019-11-19 12:33:47','2019-12-19','2019-11-21 15:08:56','Cartão','Pago','2019-11-19 12:33:47','2019-11-21 15:08:56',1),(14,50,'2019-11-19 12:33:52','2019-12-19',NULL,NULL,'Em aberto','2019-11-19 12:33:52','2019-11-19 12:33:52',1),(15,50,'2019-11-19 12:33:57','2019-12-19',NULL,NULL,'Em aberto','2019-11-19 12:33:57','2019-11-19 12:33:57',1),(16,50,'2019-11-19 12:34:02','2019-11-18',NULL,NULL,'Em atraso','2019-11-19 12:34:02','2019-11-19 12:34:02',1),(18,50,'2019-11-19 12:34:12','2019-11-17',NULL,NULL,'Em atraso','2019-11-19 12:34:12','2019-11-19 12:34:12',1),(20,50,'2019-11-19 12:34:20','2019-09-19',NULL,NULL,'Em atraso','2019-11-19 12:34:20','2019-11-19 12:34:20',1),(25,43.5,'2019-11-24 14:02:49','2019-11-24','2019-11-24 14:02:49','Pago','Pago','2019-11-24 14:02:49','2019-11-24 14:02:49',2),(26,42.5,'2019-12-01 13:14:21','2019-12-01','2019-12-01 13:14:21','Cartão','Pago','2019-12-01 13:14:21','2019-12-01 13:14:21',3);
 /*!40000 ALTER TABLE `mensalidades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,7 +235,7 @@ CREATE TABLE `pacotes` (
 
 LOCK TABLES `pacotes` WRITE;
 /*!40000 ALTER TABLE `pacotes` DISABLE KEYS */;
-INSERT INTO `pacotes` VALUES (1,'Nenhum','Não possui descontos',0,'2019-11-12 13:59:34','2019-11-12 13:59:34'),(2,'Verão','Pacote especial de verão',15,'2019-11-12 13:59:34','2019-11-12 13:59:34');
+INSERT INTO `pacotes` VALUES (1,'Nenhum','Nenhum deseconto',0,'2019-11-12 13:59:34','2019-11-12 13:59:34'),(2,'Verão','Pacote especial de verão',15,'2019-11-12 13:59:34','2019-11-12 13:59:34');
 /*!40000 ALTER TABLE `pacotes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -250,7 +255,7 @@ DELIMITER ;;
 /*!50003 SET sql_mode              = 'STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER' */ ;;
 /*!50003 SET @saved_time_zone      = @@time_zone */ ;;
 /*!50003 SET time_zone             = 'SYSTEM' */ ;;
-/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `eventGerarMensalidades` ON SCHEDULE EVERY 5 MINUTE STARTS '2019-11-16 10:05:59' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'Responsável por gerar mensalidades mensalmente' DO BEGIN
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `eventGerarMensalidades` ON SCHEDULE EVERY 15 MINUTE STARTS '2019-11-19 12:42:15' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'Responsável por gerar mensalidades mensalmente' DO BEGIN
   START TRANSACTION;
     SET @minID = (SELECT MIN(id) FROM clientes);
     SET @maxID = (SELECT MAX(id) FROM clientes);
@@ -347,7 +352,7 @@ BEGIN
 	SET @valorMensalidade = (@valor - @taxaDesconto / 100 * @valor);
   SET @forma = forma;
 	INSERT INTO Mensalidades(valor, dataEmissao, dataVencimento, dataPagamento, formaPagamento, status, createdAt, updatedAt, clienteID) 
-    VALUES (@valorMensalidade, now(), curdate(), now(), 'Pago', @forma, now(), now(), @ultimoID);
+    VALUES (@valorMensalidade, now(), curdate(), now(), @forma, 'Pago', now(), now(), @ultimoID);
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -364,4 +369,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-18 10:50:00
+-- Dump completed on 2019-12-02  2:47:34
