dd525d318a2b7abd5c07b17022fef517be183fd6
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-03 14:08:42 -0300

    .

--- email/send.js
+++ email/send.js
@@ -14,21 +14,12 @@
 
 // var $destinatario = 'leonardocamacan@gmail.com';
 
-<<<<<<< HEAD
-var mailOptions = {
-    from: $usuario,
-    to: $destinatario,
-    subject: 'Mensalidade paga com sucesso!! ',
-    text: 'Tudo certo',
-    html: '<img src="logo.png">'
-=======
 // var mailOptions = {
 //     from: $usuario,
 //     to: $destinatario,
 //     subject: 'Mensalidade paga com sucesso!! ',
 //     text: 'Tudo certo',
 //     html: '<h1 style="color: blue; ">Olá, identificamos o pagamento refente ao mês de Dezembro. OBRIGADO!!!!!</h1>'
->>>>>>> 2a462c65f94ab0a070098967d7efbdd59b6356ed
 
 // };
 
