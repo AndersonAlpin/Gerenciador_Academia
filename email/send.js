fd1f498c37ebaad4ebe2f6bfadec0f197b8ef421
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-03 11:27:58 -0300

    Carousel da home e relatórios

--- email/send.js
+++ email/send.js
@@ -1,32 +1,32 @@
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
-var $usuario = 'academiaprojetoweb@gmail.com';
-var $senha = 'Web123321'; 
+// var $usuario = 'academiaprojetoweb@gmail.com';
+// var $senha = 'Web123321'; 
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: $usuario,
-        pass: $senha
-    }
-});
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: $usuario,
+//         pass: $senha
+//     }
+// });
 
 
-var $destinatario = 'leonardocamacan@gmail.com';
+// var $destinatario = 'leonardocamacan@gmail.com';
 
-var mailOptions = {
-    from: $usuario,
-    to: $destinatario,
-    subject: 'Mensalidade paga com sucesso!! ',
-    text: 'Tudo certo',
-    html: '<h1 style="color: blue; ">Olá, identificamos o pagamento refente ao mês de Dezembro. OBRIGADO!!!!!</h1>'
+// var mailOptions = {
+//     from: $usuario,
+//     to: $destinatario,
+//     subject: 'Mensalidade paga com sucesso!! ',
+//     text: 'Tudo certo',
+//     html: '<h1 style="color: blue; ">Olá, identificamos o pagamento refente ao mês de Dezembro. OBRIGADO!!!!!</h1>'
 
-};
+// };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email enviado: ' + info.response);
-    }
-});
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email enviado: ' + info.response);
+//     }
+// });
