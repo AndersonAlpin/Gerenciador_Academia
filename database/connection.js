3af4f8d23be8e76bea29ec8cdb1d25053cacf409
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-08 10:46:58 -0300

    base de dados online

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('gerenacademia', 'gerenacademia', 'academia', {
     host: 'mysql669.umbler.com',
     dialect: 'mysql',
-    timezone: "-03:00"
+    timezone: "-03:00",
+    port: 41890
 });
 
 module.exports = connection;
