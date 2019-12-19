88633e37e46e46a7177458f3d65fd8b75d5de6c5
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-12-18 22:09:03 -0300

    Adicionado tabela "EnderecoAcademia"

--- models/MensalidadeAcademia.js
+++ models/MensalidadeAcademia.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/connection");
-const Academia = require("../models/Academia"); // Importando Cliente para gerar chave estrangeira
+const Academia = require("../models/Academia"); // Importando Academia para gerar chave estrangeira
 
-// Tabela Mensalidade
+// Tabela MensalidadeAcademia
 const MensalidadeAcademia = connection.define('mensalidadeacademia', {
     id: {
         type: Sequelize.INTEGER,
@@ -38,11 +38,11 @@ const MensalidadeAcademia = connection.define('mensalidadeacademia', {
     }
 });
 
-// Gerando chave estrangeira de Cliente na tabela Mensalidade
+// Gerando chave estrangeira de Academia na tabela MensalidadeAcademia
 Academia.hasMany(MensalidadeAcademia);
 MensalidadeAcademia.belongsTo(Academia);
 
 // Necessário para criar a base de dados. Comentar este trecho após executar o servidor
-// Mensalidade.sync({force: true});
+// MensalidadeAcademia.sync({force: true});
 
 module.exports = MensalidadeAcademia;
