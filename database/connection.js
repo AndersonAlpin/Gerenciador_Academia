688467fc436700c4ccddcc3fac30e66ca1c81dbf
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-02-04 15:27:21 -0300

    Updated

--- database/connection.js
+++ database/connection.js
@@ -6,18 +6,18 @@ const Sequelize = require("sequelize");
 //     // timezone: "-03:00"
 // });
 
-const connection = new Sequelize('sublimeteste', 'sublimeteste', '542xB7aF', {
-    host: 'mysql669.umbler.com',
-    dialect: 'mysql',
-    timezone: "-03:00",
-    port: 41890
-});
-
-// const connection = new Sequelize('sublimemanager', 'innovation', '74abN1729.X#', {
+// const connection = new Sequelize('sublimeteste', 'sublimeteste', '542xB7aF', {
 //     host: 'mysql669.umbler.com',
 //     dialect: 'mysql',
 //     timezone: "-03:00",
 //     port: 41890
 // });
+
+const connection = new Sequelize('sublimemanager', 'innovation', '74abN1729.X#', {
+    host: 'mysql669.umbler.com',
+    dialect: 'mysql',
+    timezone: "-03:00",
+    port: 41890
+});
 
 module.exports = connection;
