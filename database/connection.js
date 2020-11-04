cb6ed1b3dd41888ff769323c298b64ce3d876322
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-11-04 12:47:38 -0300

    Final

--- database/connection.js
+++ database/connection.js
@@ -1,23 +1,9 @@
 const Sequelize = require("sequelize");
 
-// const connection = new Sequelize('sublimeteste', 'root', '', {
-//     // host: 'localhost',
-//     // dialect: 'mysql',
-//     // timezone: "-03:00"
-// });
-
-// const connection = new Sequelize('sublimeteste', 'sublimeteste', '542xB7aF', {
-//     host: 'mysql669.umbler.com',
-//     dialect: 'mysql',
-//     timezone: "-03:00",
-//     port: 41890
-// });
-
-const connection = new Sequelize('sublimemanager', 'innovation', '74abN1729.X#', {
-    host: 'mysql669.umbler.com',
+const connection = new Sequelize('sublimeteste', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql',
-    timezone: "-03:00",
-    port: 41890
+    timezone: "-03:00"
 });
 
 module.exports = connection;
--- email/send.js
+++ email/send.js
@@ -5,41 +5,22 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: "startupinnovation2020@gmail.com",
-        pass: "20Inno20vation"
+        user: "emailfake@gmail.com",
+        pass: "00000"
     }
 });
-
-// transporter.sendMail({
-//     from: "Academia IronFit <academiaprojetoweb@gmail.com>",
-//     to: "anderalpin@gmail.com",
-//     subject: "Este é um email de teste",
-//     text: "Esse é um email de teste novamente",
-//     html: "Esse é um email de teste mais uma vez",
-//     attachments: [
-//         {
-//             filename: 'bemvindo.jpg',
-//             path: __dirname + '/bemvindo.jpg'
-//         }
-//     ]
-// }).then(message => {
-//     console.log(message);
-// }).catch(err => {
-//     console.log(err);
-// });
-
 
 module.exports = {
 
     enviarLink: function (email) {
         transporter.sendMail({
-            from: "Sublime <startupinnovation2020@gmail.com>",
+            from: "Sublime <emailfake@gmail.com>",
             to: email,
             subject: "Link de Recuperação de Senha",
             text: "",
             html: `
                 <h2>Para alterar sua senha, acesse o link abaixo, o link expira em 10 minutos.</h2>
-                <a href="https://gerenciaracademia.com.br/administrador/password/reset">Alterar senha</a>
+                <a href="http://localhost/administrador/password/reset">Alterar senha</a>
             `
         }).then(message => {
             console.log(message);
